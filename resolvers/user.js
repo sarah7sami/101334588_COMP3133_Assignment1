25b08d688c19ab67f9fa22aa733bf23f13f1978b
Sarah Sami <sarah7sami@gmail.com>
2023-02-19 18:12:40 -0500

    Add login to user resolver

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,6 +1,23 @@
 const UserModel = require("../models/User.js");
 
 const userResolvers = {
+  Query: {
+    login: async (_, args) => {
+      try {
+        const user = await UserModel.findOne({ username: args.username });
+        if (!user) {
+          throw new Error("User does not exist");
+        }
+        const isMatch = await user.validatePassword(args.password);
+        if (!isMatch) {
+          throw new Error("Invalid password");
+        }
+        return user;
+      } catch (err) {
+        throw err;
+      }
+    },
+  },
   Mutation: {
     signup: async (_, args) => {
       try {
